13ebda64c7e0531cd34d1cb3397ec9060075b1a8
harishnitthin <harishnitthin4@gmail.com>
2025-10-13 11:27:10 +0530

    github-setup

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import CoinContextProvider from './context/CoinContext.jsx'
 import Footer from './components/footer/Footer.jsx'
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <CoinContextProvider>
       <div className='app'>
         <NavBar />
